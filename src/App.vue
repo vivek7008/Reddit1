1fcb01c8e1ef84aaca995d3653d0bdb7d21137be
vivek <1mivivek@gmail.com>
2021-09-24 09:17:02 +0530

    Add existing file

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+<div id="app">
+
+</div>
+  
+</template>
+
+<script>
+
+
+const snoowrap = require('snoowrap');
+
+const r = new snoowrap({
+  userAgent: 'Vivek-Shinde00',
+  clientId: 'y0BuNnmycbQe-DLy3arWug',
+  clientSecret: '	OHS7racZqCoShfAwACDGoEvREVX3kg',
+  refreshToken: '1139991457823-xCSDimBNAOSyhpmZb_OKivQXvj7ISA',
+ 
+});
+
+r.getHot().map(post => post.title).then(console.log);
+r.config({ debug: true });
+
+</script>
+
+<style scoped>
+
+
+</style>
